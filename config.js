0c053f19929e2294e0a8dd505a7a79f0a033afe9
gcamon <gcamon>
2017-06-16 15:44:14 +0100

    new features

--- config.js
+++ config.js
@@ -54,18 +54,8 @@ var configuration = function (app,model) {
 		res.header('Access-Control-Allow-Origin', req.headers.host);
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-		if ('OPTIONS' == req.method) {
-		     res.send(200);
-		 } else {
-		     next();
-		 }
 	});
 
-	app.use(function(req,res,next){
-		console.log(req.user);
-		next()
-	})
- 
 	
 	passport.serializeUser(function(user, done) {    
     done(null, user._id);
